38b87365eca8f8313475741b776cf24bfcfaf91a
Zac Ong <zac.ong@ibm.com>
2022-07-04 20:56:32 +1000

    feat: setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
-
-const App: Component = () => {
+export default function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class="text-center">
+      <header class="flex min-h-screen justify-center items-center">
+        <h1 class="text-5xl font-extrabold">Hello Solid.js</h1>
       </header>
     </div>
-  );
-};
-
-export default App;
+  )
+}
