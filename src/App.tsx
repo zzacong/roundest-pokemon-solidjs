a7f1418e2259d20f4d82c857f3f04ea2310df4cf
Zac Ong <zac.ong@ibm.com>
2022-07-04 22:23:20 +1000

    use switch and match for loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Show } from 'solid-js'
+import { Match, Show, Switch } from 'solid-js'
 import { LoadingSpinner, PokemonVoting } from './components'
 import { createTrpcMutation, createTrpcQuery } from './lib/trpc'
 
@@ -14,26 +14,31 @@ export default function App() {
   return (
     <div class="flex grow flex-col items-center justify-center px-6 pt-16">
       <h1 class="mb-12 text-center text-lg font-bold md:text-2xl lg:mb-20 lg:text-4xl">
-        Which Pokemon is Roundest?
+        Which Pokemon is Roundest? {String(pair.loading)}
       </h1>
 
-      <Show when={pair()} fallback={<LoadingSpinner />}>
-        {({ firstPokemon, secondPokemon }) => (
-          <div class="flex max-w-2xl flex-col items-center justify-between gap-6 rounded border py-8 px-8 md:flex-row lg:gap-10 lg:px-16">
-            <PokemonVoting
-              pokemon={firstPokemon}
-              vote={voteForRoundest(firstPokemon.id, secondPokemon.id)}
-              // disabled={voteMutation.isLoading || isLoading}
-            />
-            <div class="font-mono text-2xl">VS</div>
-            <PokemonVoting
-              pokemon={secondPokemon}
-              vote={voteForRoundest(secondPokemon.id, firstPokemon.id)}
-              // disabled={voteMutation.isLoading || isLoading}
-            />
-          </div>
-        )}
-      </Show>
+      <Switch fallback={<LoadingSpinner />}>
+        <Match when={!pair.loading && !pair()}>
+          <LoadingSpinner message="no pokemon to show" />
+        </Match>
+        <Match when={pair()}>
+          {({ firstPokemon, secondPokemon }) => (
+            <div class="flex max-w-2xl flex-col items-center justify-between gap-6 rounded border py-8 px-8 md:flex-row lg:gap-10 lg:px-16">
+              <PokemonVoting
+                pokemon={firstPokemon}
+                vote={voteForRoundest(firstPokemon.id, secondPokemon.id)}
+                // disabled={voteMutation.isLoading || isLoading}
+              />
+              <div class="font-mono text-2xl">VS</div>
+              <PokemonVoting
+                pokemon={secondPokemon}
+                vote={voteForRoundest(secondPokemon.id, firstPokemon.id)}
+                // disabled={voteMutation.isLoading || isLoading}
+              />
+            </div>
+          )}
+        </Match>
+      </Switch>
     </div>
   )
 }
