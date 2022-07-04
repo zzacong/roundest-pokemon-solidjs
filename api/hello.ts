d4f5da400a00db2b5aae3cf1fa0717b2afc1c8f5
Zac Ong <zac.ong@ibm.com>
2022-07-04 21:18:22 +1000

    vercel api

---
+++ api/hello.ts
@@ -0,0 +1,5 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  return res.status(200).json({ message: 'hello' })
+}
