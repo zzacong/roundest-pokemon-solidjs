d20a5bfdac32092c59c6d189d4fe190dd241fa22
Zac Ong <zac.ong@ibm.com>
2022-07-04 21:50:53 +1000

    call trpc query for the fist time in solidjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { createTrpcQuery } from './lib/trpc'
+
 export default function App() {
+  const [pair, { refetch }] = createTrpcQuery('get-pokemon-pair')
+
   return (
     <div class="text-center">
-      <header class="flex min-h-screen justify-center items-center">
+      <header class="flex flex-col min-h-screen justify-center items-center px-10">
         <h1 class="text-5xl font-extrabold">Hello Solid.js</h1>
+        <code class="break-all font-mono">{JSON.stringify(pair())}</code>
       </header>
     </div>
   )
