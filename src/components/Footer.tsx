15580ece91c2dc7c132eba3b73c14f442ecbcd6e
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-11-26 14:20:57 +1100

    with pnpm, remove vercel.json

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer class="py-6">
       <nav class="mt-10 flex items-center justify-center gap-8 font-mono">
         <a
-          href="https://github.com/zzacong/roundest-pokemon-nextjs"
+          href="https://github.com/zzacong/roundest-pokemon-solidjs"
           target="_blank"
           rel="noopener noreferrer"
           class="text-base font-semibold hover:underline hover:opacity-80"
